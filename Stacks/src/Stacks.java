b1733e1a12418fe6e39b2b51bbc65820825d2662
Kartik Sayee <kartiksayee@Kartiks-MacBook-Pro-2.local>
2016-02-07 22:38:36 -0800

    codes

---
+++ Stacks/src/Stacks.java
@@ -0,0 +1,75 @@
+// Write a program to implement a stack.
+
+public class Stacks {
+	
+	int top;
+	int size;
+	int[] stk;
+	
+	public Stacks(int size)
+	{
+		top=-1;
+		this.size=size;
+		stk=new int[size];
+	}
+	
+	public void push(int data)
+	{
+		if(top==-1)
+		{
+			top++;
+			stk[top]=data;
+		}
+		else if(top+1==size)
+		{
+			System.out.println("Stack is full");
+		}
+		else
+		{
+			top++;
+			stk[top]=data;
+		}
+	}
+	
+	public void pop()
+	{
+		if(top==-1)
+			System.out.println("Stack is empty");
+		else
+			top--;
+	}
+	
+	public void display()
+	{
+		if(top==-1)
+			System.out.println("Stack is empty");
+		else
+		{
+			for(int i=0;i<=top;i++)
+			{
+				System.out.println(stk[i]);
+			}
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		Stacks stk=new Stacks(5);
+		System.out.println("Adding Elemnts");
+		stk.push(10);
+		stk.push(20);
+		stk.push(30);
+		stk.push(40);
+		stk.push(50);
+		System.out.println("Display Elements");
+		stk.display();
+		System.out.println("Popping Elemnts");
+		stk.pop();
+		stk.pop();
+		stk.pop();
+		stk.pop();
+		stk.pop();
+		System.out.println("Display Elemnts");
+		stk.display();
+	}
+}
