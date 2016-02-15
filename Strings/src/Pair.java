77322d3aa5a73bfc20f4d0e3a881d777ab4d780e
Kartik Sayee <kartiksayee@Kartiks-MacBook-Pro-2.local>
2016-02-14 17:52:41 -0800

    codes

---
+++ Strings/src/Pair.java
@@ -0,0 +1,13 @@
+// Write a program check if 2 strings are Anagrams or NOT.
+
+public class Pair
+{
+	int first;
+	int second;
+	
+	public Pair(int i, int j)
+	{
+		this.first=i;
+		this.second=j;
+	}
+}
