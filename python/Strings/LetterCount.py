f4b7ad1dcad7b9613c413a47bbb7e4fed63dbfdd
ksayee <kartiksayee@gmail.com>
2016-11-19 15:25:22 -0800

    codes

--- python/Strings/LetterCount.py
+++ python/Strings/LetterCount.py
@@ -1,19 +1,22 @@
 def lettercount(str1):
 
     count=0
+    lst={}
     for i in range(0,len(str1)):
-        if (str1[i]>='a' and str1[i]<='z') or (str1[i]>='A' and str1[i]<='Z'):
-            count=count+1
-    return count
+        if str1[i]!=' ' and ((str1[i]>='a' and str1[i]<='z') or (str1[i]>='A' and str1[i]<='Z')):
+            key=str1[i]
+            if key in lst.keys():
+                lst[key]=lst.get(key)+1
+            else:
+                lst[key]=1
+
+    for key,val in lst.items():
+        print(key,val)
 
 def main():
     str1 = 'I work in Amazon in Seattle'
-    str2 = '    I    work * * %    in    Amazon     in Seattle'
-    str3 = '*%I     work in    Amazon in     Seattle   $    '
 
-    print("Count of letters is: ",lettercount(str1))
-    print("Count of letters is: ",lettercount(str2))
-    print("Count of letters is: ",lettercount(str3))
+    lettercount(str1)
 
 
 if __name__=='__main__':
