f86898d4034fa9ff7cd83c9ead974361a6e4e0d2
ksayee <kartiksayee@gmail.com>
2016-11-26 21:50:43 -0800

    codes

---
+++ python/Strings/PowerofTwo.py
@@ -0,0 +1,23 @@
+import math
+
+def poweroftwo(n):
+
+    if n%2!=0:
+        print('Number NOT power of two')
+    else:
+        while n!=1:
+            n=int(n/2)
+            if n%2!=0:
+                break
+        if n==1:
+            print('Number is power of two')
+        else:
+            print('Number NOT power of two')
+
+
+def main():
+    n=128
+    poweroftwo(n)
+
+if __name__=='__main__':
+    main()
