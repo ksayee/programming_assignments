0e7caec7d2d97799097d16d5e1d629687d724c74
Kartik Sayee <kartiksayee@gmail.com>
2018-08-05 21:03:32 -0700

    New Codes

--- python/CodingExercises/Multiplication.py
+++ python/CodingExercises/Multiplication.py
@@ -10,41 +10,59 @@ def Multiplication(s1,s2):
 
     carry_a=0
     carry_p=0
+    flg=True
     fnl_lst=[]
 
     for i in range(len(lst2)-1,-1,-1):
-        k=(len(lst2)-1)-i
         tmp2=int(lst2[i])
+        k=(len(lst2)-1)-i
         for j in range(len(lst1)-1,-1,-1):
             tmp1=int(lst1[j])
+
             if k==0:
-                num=carry_a+(tmp1*tmp2)
+                num=carry_a+(tmp2*tmp1)
                 rem=num%10
-                carry_a=int(num/10)
                 fnl_lst.append(str(rem))
-            elif k!=0:
+                carry_a=int(num/10)
+            else:
                 num1=carry_a+(tmp1*tmp2)
-                num=carry_p + int(fnl_lst[k])+num1%10
+
+                if flg==True:
+                    num=carry_p+int(fnl_lst[k])+(num1%10)
+                else:
+                    num=carry_p+(num1%10)
+
                 rem=num%10
+                carry_a = int(num1 / 10)
                 carry_p=int(num/10)
-                fnl_lst[k]=str(rem)
-                carry_a=int(num1/10)
+                if k<=len(fnl_lst)-1 and flg==True:
+                    fnl_lst[k]=str(rem)
+
+                if flg == False:
+                    fnl_lst.append(str(rem))
+                    k=k+1
+                    carry_p=0
 
                 if k<len(fnl_lst)-1:
                     k=k+1
-        if k!=0:
-            if k<=(len(fnl_lst)-1) and (carry_a+carry_p)!=0:
-                fnl_lst.append(str(carry_p+carry_a))
-        else:
+                else:
+                    flg=False
+
+        if k==0:
             if carry_a!=0:
-                fnl_lst.append(carry_a)
-            carry_a=0
+                fnl_lst.append(str(carry_a))
+        else:
+            if carry_a+carry_p!=0:
+                fnl_lst.append(str(carry_p+carry_a))
+        flg=True
+        carry_a=0
+        carry_p=0
 
     fnl_lst.reverse()
     return ''.join(fnl_lst)
 
 def main():
-    
+
     s1='25'
     s2='76'
     print(Multiplication(s1,s2))
@@ -57,6 +75,13 @@ def main():
     s2='89'
     print(Multiplication(s1,s2))
 
+    s1 = '123'
+    s2 = '456'
+    print(Multiplication(s1, s2))
+
+    s1 = '99'
+    s2 = '88888'
+    print(Multiplication(s1, s2))
 
 if __name__=='__main__':
     main()
