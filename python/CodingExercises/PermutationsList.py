65e8ee24462e5b399dd542e6f6bc4dc60541d2a0
Kartik Sayee <kartiksayee@gmail.com>
2018-09-04 20:50:12 -0700

    New Codes

--- python/CodingExercises/PermutationsList.py
+++ python/CodingExercises/PermutationsList.py
@@ -1,44 +1,41 @@
-# Permutations of a string aabc
+# Permutations of a list lst=['us','uk','in']
 
 import collections
 
-def permutation(str1):
+def PermutationsList(ary):
 
-    dict=collections.Counter(str1)
+    dict=collections.Counter(ary)
 
     lst=[]
     cnt=[]
+
     for key,val in dict.items():
         lst.append(key)
         cnt.append(val)
+
     res=[]
     fnl_lst=[]
-    permutation_recur(lst,cnt,res,len(str1),fnl_lst)
-
+    PermutationsList_recur(lst,cnt,res,len(ary),fnl_lst)
     return fnl_lst
 
-def permutation_recur(lst,cnt,res,lgt,fnl_lst):
+def PermutationsList_recur(lst,cnt,res,lgt,fnl_lst):
 
     if len(res)==lgt:
-        fnl_lst.append(''.join(res))
+        fnl_lst.append(res.copy())
 
     for i in range(0,len(lst)):
         if cnt[i]==0:
             continue
         res.append(lst[i])
         cnt[i]=cnt[i]-1
-        permutation_recur(lst, cnt, res,lgt, fnl_lst)
+        PermutationsList_recur(lst, cnt, res, lgt, fnl_lst)
         res.pop()
         cnt[i]=cnt[i]+1
 
 def main():
 
-    str1='aabc'
-    print(permutation(str1))
-
-    str1 = '1233'
-    print(permutation(str1))
-
+    ary=['us', 'uk', 'in']
+    print(PermutationsList(ary))
 
 if __name__=='__main__':
     main()
