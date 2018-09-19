409adc1c6a5225d160d5d7ee85b20320f1f00737
Kartik Sayee <kartiksayee@gmail.com>
2018-09-18 21:05:52 -0700

    New Codes

--- python/CodingExercises/LeetCode172.py
+++ python/CodingExercises/LeetCode172.py
@@ -15,15 +15,38 @@ Output: 1
 Explanation: 5! = 120, one trailing zero.
 '''
 
+def Factorial(n):
+
+    if n==0:
+        return 1
+    return n*Factorial(n-1)
 
 def LeetCode172(n):
 
-    
+    num=Factorial(n)
+
+    found=False
+    cnt=0
+    while found!=True:
+
+        rem=num%10
+        if rem==0:
+            if found==False:
+                cnt=cnt+1
+        else:
+            found=True
+        num=num//10
+
+    return cnt
 
 def main():
 
     n=3
     print(LeetCode172(n))
+
+    n = 5
+    print(LeetCode172(n))
+
 
 if __name__=='__main__':
     main()
