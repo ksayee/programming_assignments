53560e42372e055e91c62dffe9c777d795dd68a0
Kartik Sayee <kartiksayee@gmail.com>
2018-09-18 20:32:47 -0700

    New Codes

---
+++ python/CodingExercises/LeetCode172.py
@@ -0,0 +1,29 @@
+'''
+172. Factorial Trailing Zeroes
+
+Given an integer n, return the number of trailing zeroes in n!.
+
+Example 1:
+
+Input: 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+'''
+
+
+def LeetCode172(n):
+
+    
+
+def main():
+
+    n=3
+    print(LeetCode172(n))
+
+if __name__=='__main__':
+    main()
