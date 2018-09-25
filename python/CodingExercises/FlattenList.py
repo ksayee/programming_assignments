d441bab293a3d3225f95dbee23346427f6eece37
Kartik Sayee <kartiksayee@gmail.com>
2018-09-24 21:34:36 -0700

    New Codes

--- python/CodingExercises/FlattenList.py
+++ python/CodingExercises/FlattenList.py
@@ -1,25 +1,29 @@
-def flattenStack(lst):
-    stk=[]
-    for i in range(len(lst)-1,-1,-1):
-        stk.append(lst[i])
-    print(stk)
-    stk.pop()
-    print(stk)
+'''
+'''
 
 def flattenlist(lst):
-    fnl=[]
+
+    fnl_lst=[]
+    fnl_lst=flattenlist_recur(lst)
+
+    return fnl_lst
+
+def flattenlist_recur(lst):
+
+    fnl_lst=[]
+
     for l in lst:
         if isinstance(l,list):
-            fnl.extend(flattenlist(l))
+            fnl_lst.extend(flattenlist_recur(l))
         else:
-            fnl.append(l)
-    return fnl
+            fnl_lst.append(l)
+    return fnl_lst
+
 
 def main():
     
     lst=[1,[2,3,4,5],6,7,[8,9,[10,11]]]
     print(flattenlist(lst))
-    flattenStack(lst)
 
 if __name__=='__main__':
     main()
