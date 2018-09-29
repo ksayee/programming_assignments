b0574f22c73252e496a55b8568dc4e35dd1eb443
Kartik Sayee <kartiksayee@gmail.com>
2018-09-29 15:26:02 -0700

    New Codes

--- python/CodingExercises/LeetCode456.py
+++ python/CodingExercises/LeetCode456.py
@@ -1,6 +1,7 @@
 '''
 456. 132 Pattern
-Given a sequence of n integers a1, a2, ..., an, a 132 pattern is a subsequence ai, aj, ak such that i < j < k and ai < ak < aj. Design an algorithm that takes a list of n numbers as input and checks whether there is a 132 pattern in the list.
+Given a sequence of n integers a1, a2, ..., an, a 132 pattern is a subsequence ai, aj, ak such that i < j < k and ai < ak < aj.
+Design an algorithm that takes a list of n numbers as input and checks whether there is a 132 pattern in the list.
 
 Note: n will be less than 15,000.
 
